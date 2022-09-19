7755abdf7861c379cecfc0d715195ddcafac5372
Lizzy Wiley <Lizzyrho@gmail.com>
2022-09-19 14:48:14 -0500

    added bare bones react app

---
+++ ls/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Show now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Show now</p>
+        </div>
+      </div> 
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Show now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Women</h2>
+          <p>Show now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Men</h2>
+          <p>Show now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
